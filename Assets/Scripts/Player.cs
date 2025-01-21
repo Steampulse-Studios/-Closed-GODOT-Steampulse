3a58d3df97e557e80c76f460fed8412f22a774f5
CatanRuler2006 <158619088+CatanRuler2006@users.noreply.github.com>
2025-01-21 14:05:03 -0600

    Add files via upload

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed;
+    float speedX, speedY;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        speedX = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        speedY = Input.GetAxisRaw("Vertical") * moveSpeed;
+        rb.velocity = new Vector2(speedX, speedY);
+    }
+}
